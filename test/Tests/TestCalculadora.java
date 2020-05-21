c9565f2fd863105713f81eabf367ddc1f6d2be90
Mat <matrj08@gmail.com>
2020-05-20 20:16:03 -0600

    New tests functions

--- test/Tests/TestCalculadora.java
+++ test/Tests/TestCalculadora.java
@@ -15,38 +15,45 @@ import CalculadoraPck.main;
  * @author Keons
  */
 public class TestCalculadora {
-    main calculadora = new main();
-   @Test
-   public void TestSuma(){
-       assertEquals(4,calculadora.suma(2, 2));
-   }
-   @Test
-   public void TestResta(){
-       assertEquals(3,calculadora.resta(5, 2));
-   }
-   @Test
-   public void TestDivision(){
-       assertEquals(5,calculadora.division(10, 2));
-   }
-   @Test
-   public void TestMultiplicacion(){
-       assertEquals(8,calculadora.multiplicacion(4, 2));
-   }
-   @Test
-   public void TestSuma2(){
-       assertEquals(15,calculadora.suma(10, 5));
-   }
-   @Test
-   public void TestResta2(){
-       assertEquals(-4,calculadora.resta(4, 8));
-   }
-   @Test
-   public void TestDivision2(){
-       assertEquals(100/3,calculadora.division(100, 3));
-   }
-   @Test
-   public void TestMultiplicacion2(){
-       assertEquals(18,calculadora.multiplicacion(9, 2));
-   }
+    @Test
+    public void TestSuma(){
+        main calculadora = new main();
+        assertEquals(4,calculadora.suma(2, 2));
+    }
+    @Test
+    public void TestResta(){
+        main calculadora = new main();
+        assertEquals(3,calculadora.resta(5, 2));
+    }
+    @Test
+    public void TestDivision(){
+        main calculadora = new main();
+        assertEquals(5,calculadora.division(10, 2));
+    }
+    @Test
+    public void TestMultiplicacion(){
+        main calculadora = new main();
+        assertEquals(8,calculadora.multiplicacion(4, 2));
+    }
+    @Test
+    public void TestSuma2(){
+        main calculadora = new main();
+        assertEquals(15,calculadora.suma(10, 5));
+    }
+    @Test
+    public void TestResta2(){
+        main calculadora = new main();
+        assertEquals(-4,calculadora.resta(4, 8));
+    }
+    @Test
+    public void TestDivision2(){
+        main calculadora = new main();
+        assertEquals(100/3,calculadora.division(100, 3));
+    }
+    @Test
+    public void TestMultiplicacion2(){
+        main calculadora = new main();
+        assertEquals(18,calculadora.multiplicacion(9, 2));
+    }
     
 }
