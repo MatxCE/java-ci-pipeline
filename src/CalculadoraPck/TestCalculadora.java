1f3a2c89b2209c1839f178dcd5a25fcdcb06ee14
Mat <matrj08@gmail.com>
2020-05-20 21:19:36 -0600

    Code error fixed

--- src/CalculadoraPck/TestCalculadora.java
+++ src/CalculadoraPck/TestCalculadora.java
@@ -3,12 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Tests;
+package CalculadoraPck;
 
 
 import org.junit.Test;
 import static org.junit.Assert.*;
-import CalculadoraPck.main;
 
 /**
  *
